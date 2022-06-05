5f169929d95af1de927d2e78fdf7c350f68f432b
cmv <68516521+cm-verbose@users.noreply.github.com>
2022-06-05 12:58:12 -0400

    Update qdf.ts

--- Typescript/Math/qdf.ts
+++ Typescript/Math/qdf.ts
@@ -1,5 +1,1 @@
-let qdf = (a:number, b:number, c:number) =>{
-  let x = (-b + Math.sqrt( Math.pow(b, 2) - (4 * (a) * (c)) )) / (2 * a), 
-  y = (-b - Math.sqrt( Math.pow(b, 2) - (4 * (a) * (c)) )) / (2 * a)
-  return [x, y]; 
-}
+let qdf = (a:number, b:number, c:number) => [(-b + Math.sqrt( Math.pow(b, 2) - (4 * (a) * (c)) )) / (2 * a), (-b - Math.sqrt( Math.pow(b, 2) - (4 * (a) * (c)) )) / (2 * a)]; 
