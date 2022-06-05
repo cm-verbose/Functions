ad70d46de3fdb0135fd66e0cf4d0c3e104f7bb31
cmv <68516521+cm-verbose@users.noreply.github.com>
2022-06-05 11:43:24 -0400

    Update lcm.ts

--- Typescript/Math/lcm.ts
+++ Typescript/Math/lcm.ts
@@ -1,4 +1,4 @@
-const lcd = (x: number, y: number): number => {
+let lcd = (x: number, y: number): number => {
   let gcd = (a: number, b: number): number => b === 0 ? a : gcd(b, a % b);
   return Math.abs(x * y) / gcd(x, y);
 }
